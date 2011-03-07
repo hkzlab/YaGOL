0d4ca737b7a0ab906f76212021204221091df9f7
Fabio Battaglia <hkzlabnet@gmail.com>
2011-03-07 15:48:40 +0100

    DRAWING: fix headers of some drawing functions
    color parameter was missing

--- src/drawing/drawing_base.c
+++ src/drawing/drawing_base.c
@@ -54,14 +54,14 @@ void dw_setPixel(SDL_Surface *s, Uint32 color, Uint16 x, Uint16 y) {
 
 }
 
-void dw_drawHLine(SDL_Surface *s, Uint16 x, Uint16 y, Uint16 len) {
+void dw_drawHLine(SDL_Surface *s, Uint32 color, Uint16 x, Uint16 y, Uint16 len) {
 	;
 }
 
-void dw_drawVLine(SDL_Surface *s, Uint16 x, Uint16 y, Uint16 len) {
+void dw_drawVLine(SDL_Surface *s, Uint32 color, Uint16 x, Uint16 y, Uint16 len) {
 	;
 }
 
-void dw_drawBox(SDL_Surface *s, Uint16 x, Uint16 y, Uint16 h, Uint16 w, Uint16 border) {
+void dw_drawBox(SDL_Surface *s, Uint32 color, Uint16 x, Uint16 y, Uint16 h, Uint16 w, Uint16 border) {
 	;
 }
