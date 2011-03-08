73724c3bcac5240f1b7f01c433ff9307955e2f50
Fabio Battaglia <hkzlabnet@gmail.com>
2011-03-08 16:37:18 +0100

    EVENTS: react at button presses

--- src/events/events_manager.c
+++ src/events/events_manager.c
@@ -1,7 +1,10 @@
 #include "events/events.h"
 #include "common/defs.h"
 
-extern Uint8 should_quit;
+extern Uint8 should_quit, should_redraw_grid, fast_forward, paused;
+extern Uint16 all_button_y, chaos_button_x, fastf_button_x, quit_button_x, start_button_x, step_button_x, stop_button_x;
+
+void button_press(SDL_MouseButtonEvent *ev);
 
 void poll_sdl_events(void) {
 	SDL_Event event;
@@ -13,6 +16,7 @@ void poll_sdl_events(void) {
 			case SDL_MOUSEBUTTONDOWN:
 				break;
 			case SDL_MOUSEBUTTONUP:
+				button_press(&(event.button));
 				break;
 			case SDL_QUIT:
 				should_quit = 1;
@@ -21,3 +25,33 @@ void poll_sdl_events(void) {
 	}
 }
 
+void button_press(SDL_MouseButtonEvent *ev) {
+	assert(ev);
+
+	if (ev->button != SDL_BUTTON_LEFT) return;
+	
+	if(ev->y >= all_button_y && ev->y <= all_button_y + BUTTON_WIDTH) { // The click might be on a button
+		if (ev->x >= quit_button_x && ev->x <= quit_button_x + BUTTON_WIDTH) // QUIT button
+			should_quit = 1;
+		else if (ev->x >= chaos_button_x && ev->x <= chaos_button_x + BUTTON_WIDTH) { // CHAOS button
+			randomize_gol();
+			should_redraw_grid = 1;
+		} else if (ev->x >= fastf_button_x && ev->x <= fastf_button_x + BUTTON_WIDTH) { // FASTF button
+			fast_forward = !fast_forward;
+			paused = 0;
+		} else if (ev->x >= start_button_x && ev->x <= start_button_x + BUTTON_WIDTH) { // START button
+			paused = 0;
+			fast_forward = 0;
+		} else if (ev->x >= step_button_x && ev->x <= step_button_x + BUTTON_WIDTH) { // STEP button
+			if (!paused) paused = 1;
+			else {
+				gol_step();
+				should_redraw_grid = 1;
+			}
+		} else if (ev->x >= stop_button_x && ev->x <= stop_button_x + BUTTON_WIDTH) { // STOP button
+			paused = 1;
+			reset_gol();
+			should_redraw_grid = 1;
+		}
+	}
+}
