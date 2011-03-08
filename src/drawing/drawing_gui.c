6942fdc6879ee6d978b46f5d5ec2a882ff874c3c
Fabio Battaglia <hkzlabnet@gmail.com>
2011-03-08 13:52:35 +0100

    DRAWING: drawControlDeck draws the whole bar now

--- src/drawing/drawing_gui.c
+++ src/drawing/drawing_gui.c
@@ -10,16 +10,20 @@
 #define STEP_BUTTON "src/resources/step_button.png"
 #define STOP_BUTTON "src/resources/stop_button.png"
 
+#define BUTTON_WIDTH 32
+
+void dw_gui_private_drawFullImage(SDL_Surface *s, char *imagepath, Uint16 x, Uint16 y);
+
 void dw_gui_drawControlDeck(SDL_Surface *s, enum HPosition hpos, enum VPosition vpos) {
 	assert(s);
 
-	Uint16 x_deck_pos, y_deck_pos;
+	Uint16 x_deck_pos, y_deck_pos, x_button_pos, y_button_pos;
+	SDL_Rect src_rect, dst_rect;
+	SDL_Surface *button;
 	SDL_Surface *main_deck = IMG_Load(CONTROL_DECK);
 
-	SDL_Rect src_rect, dst_rect;
-
 	if (!main_deck) {
-		fprintf(stderr, "dw_gui_drawControlDeck() failed: unable to load image\n");
+		fprintf(stderr, "dw_gui_drawControlDeck() failed: unable to load %s image\n", CONTROL_DECK);
 		return;
 	}
 
@@ -62,10 +66,65 @@ void dw_gui_drawControlDeck(SDL_Surface *s, enum HPosition hpos, enum VPosition 
 
 	SDL_BlitSurface(main_deck, &src_rect, s, &dst_rect);
 
+	// Time to draw the BUTTONS!
+	y_button_pos = y_deck_pos + 4;
+	x_button_pos = x_deck_pos + 4;
+
+	// START button
+	dw_gui_private_drawFullImage(s, START_BUTTON, x_button_pos, y_button_pos);
+
+	// STEP button
+	x_button_pos += BUTTON_WIDTH + 3;
+	dw_gui_private_drawFullImage(s, STEP_BUTTON, x_button_pos, y_button_pos);
+
+	// FASTF button
+	x_button_pos += BUTTON_WIDTH + 3;
+	dw_gui_private_drawFullImage(s, FASTF_BUTTON, x_button_pos, y_button_pos);
+
+	// STOP button
+	x_button_pos += BUTTON_WIDTH + 3;
+	dw_gui_private_drawFullImage(s, STOP_BUTTON, x_button_pos, y_button_pos);
+
+	// QUIT button
+	x_button_pos = (x_deck_pos + main_deck->w) - 4 - BUTTON_WIDTH;
+	dw_gui_private_drawFullImage(s, QUIT_BUTTON, x_button_pos, y_button_pos);
+
+	// CHAOS button
+	x_button_pos -= BUTTON_WIDTH + 3 ;
+	dw_gui_private_drawFullImage(s, CHAOS_BUTTON, x_button_pos, y_button_pos);
+
+	// DECK LOGO!
+	x_button_pos -= 70 ;
+	dw_gui_private_drawFullImage(s, DECK_LOGO, x_button_pos, y_button_pos);
+
+
 	// Unlock and free main deck surface
 	if (SDL_MUSTLOCK(main_deck)) SDL_UnlockSurface(main_deck);
 	SDL_FreeSurface(main_deck);
 
 	// Unlock screen surface!!!
 	if (SDL_MUSTLOCK(s)) SDL_UnlockSurface(s);
+}
+
+void dw_gui_private_drawFullImage(SDL_Surface *s, char *imagepath, Uint16 x, Uint16 y) {
+	assert(s);
+	assert(imagepath);
+
+	SDL_Surface *img;
+	SDL_Rect dst_rect, src_rect;
+
+	img = IMG_Load(imagepath);
+	if (!img) {
+		fprintf(stderr, "dw_gui_private_drawFullImage() failed: unable to load %s image\n", imagepath);
+		return;		
+	}
+
+	if (SDL_MUSTLOCK(img)) SDL_LockSurface(img);
+	src_rect.x = 0; src_rect.y = 0; src_rect.w = img->w; src_rect.h = img->h;
+	dst_rect.x = x; dst_rect.y = y; dst_rect.w = img->w; dst_rect.h = img->h;
+	SDL_BlitSurface(img, &src_rect, s, &dst_rect);
+
+	if (SDL_MUSTLOCK(img)) SDL_UnlockSurface(img);
+
+	SDL_FreeSurface(img);
 }
