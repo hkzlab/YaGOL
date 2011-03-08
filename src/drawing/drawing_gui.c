fa546f198b6debc9e47db43ec3daab1fa4f9812f
Fabio Battaglia <hkzlabnet@gmail.com>
2011-03-08 12:47:39 +0100

    DRAWING: Add some defines to find res files

--- src/drawing/drawing_gui.c
+++ src/drawing/drawing_gui.c
@@ -1,5 +1,14 @@
 #include "drawing/drawing.h"
 
+#define CONTROL_DECK "resources/control_deck.png"
+#define DECK_LOGO "resources/deck_logo.png"
+#define CHAOS_BUTTON "resources/chaos_button.png"
+#define FASTF_BUTTON "resources/fastf_button.png"
+#define QUIT_BUTTON "resources/quit_button.png"
+#define START_BUTTON "resources/start_button.png"
+#define STEP_BUTTON "resources/step_button.png"
+#define STOP_BUTTON "resources/stop_button.png"
+
 void dw_gui_drawControlDeck(SDL_Surface *s, enum HPosition vpos, enum VPosition hpos) {
 	;	
 }
