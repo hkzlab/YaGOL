9aeb668bb63eb6b6003c33b708d462a3633cdcc5
Fabio Battaglia <hkzlabnet@gmail.com>
2011-03-08 19:08:19 +0100

    DRAWING: change GoL background colors

--- src/drawing/drawing_gol.c
+++ src/drawing/drawing_gol.c
@@ -10,27 +10,27 @@ void dw_gol_drawGoLPlane(SDL_Surface *s, Uint16 x, Uint16 y, Uint16 cell_size, U
 	Uint16 x_pos, y_pos;
 
 	Uint32 color;
-	Uint32 col_black;
+	Uint32 col_dark;
 	SDL_Rect node;
 
 	if (SDL_MUSTLOCK(s)) SDL_LockSurface(s);
 
-	col_black = SDL_MapRGB(s->format, 0, 0, 0);
+	col_dark = SDL_MapRGB(s->format, 0, 0, 80);
 
 	for (x_pos = 0; x_pos < gol_width; x_pos++)
 		for (y_pos = 0; y_pos < gol_height; y_pos++) {
 			if (get_gol_node(x_pos, y_pos))
 				color = cell_color;
 			else
-				color = col_black;
+				color = col_dark;
 
 			node.w = node.h = cell_size;
 			node.x = x + x_pos * cell_size;
 			node.y = y + y_pos * cell_size;
 
 			SDL_FillRect(s, &node, color);
-			if (color != col_black) dw_drawBox(s, border_color, node.x, node.y, node.w, node.h, 1);
-			else dw_drawBox(s, SDL_MapRGB(s->format, 20, 20, 30), node.x, node.y, node.w, node.h, 1);
+			if (color != col_dark) dw_drawBox(s, border_color, node.x, node.y, node.w, node.h, 1);
+			else dw_drawBox(s, SDL_MapRGB(s->format, 0, 0, 0), node.x, node.y, node.w, node.h, 1);
 		}
 
 	if (SDL_MUSTLOCK(s)) SDL_UnlockSurface(s);
