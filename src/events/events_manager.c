548d93c3879cbf166bc15d2caa89628735b3268d
Fabio Battaglia <hkzlabnet@gmail.com>
2011-03-08 16:58:26 +0100

    EVENTS: manage clicks on the GoL board

--- src/events/events_manager.c
+++ src/events/events_manager.c
@@ -3,6 +3,7 @@
 
 extern Uint8 should_quit, should_redraw_grid, fast_forward, paused;
 extern Uint16 all_button_y, chaos_button_x, fastf_button_x, quit_button_x, start_button_x, step_button_x, stop_button_x;
+extern Uint16 cell_grid_x, cell_grid_y, cell_grid_size;
 
 void button_press(SDL_MouseButtonEvent *ev);
 
@@ -30,7 +31,7 @@ void button_press(SDL_MouseButtonEvent *ev) {
 
 	if (ev->button != SDL_BUTTON_LEFT) return;
 	
-	if(ev->y >= all_button_y && ev->y <= all_button_y + BUTTON_WIDTH) { // The click might be on a button
+	if (ev->y >= all_button_y && ev->y <= all_button_y + BUTTON_WIDTH) { // The click might be on a button
 		if (ev->x >= quit_button_x && ev->x <= quit_button_x + BUTTON_WIDTH) // QUIT button
 			should_quit = 1;
 		else if (ev->x >= chaos_button_x && ev->x <= chaos_button_x + BUTTON_WIDTH) { // CHAOS button
@@ -55,6 +56,21 @@ void button_press(SDL_MouseButtonEvent *ev) {
 		}
 
 		return;
+	}
+
+	if (ev->y >= cell_grid_y && ev->y <= cell_grid_y + (DEFAULT_GRID_HEIGHT * cell_grid_size) && ev->x >= cell_grid_x && ev->x <= cell_grid_x + (DEFAULT_GRID_WIDTH * cell_grid_size)) { // A click on the grid!!!
+		if (!paused) return;
+
+		Uint16 relative_x = ev->x - cell_grid_x;
+		Uint16 relative_y = ev->y - cell_grid_y;
+
+		Uint16 cell_x = relative_x / cell_grid_size;
+		Uint16 cell_y = relative_y / cell_grid_size;
+
+		Uint8 node = get_gol_node(cell_x, cell_y);
+		set_gol_node(!node, cell_x, cell_y);
+
+		should_redraw_grid = 1;
 	}
 
 	// Now i should manage clicks on the GoL board
